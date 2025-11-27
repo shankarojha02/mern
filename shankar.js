2e3cc4a1a4378ee8929f1c5341899f35163284b7
shankarojha02 <shankarojha02@gmail.com>
2025-11-27 18:53:41 +0545

    next.html

---
+++ shankar.js
@@ -0,0 +1,58 @@
+// let a = 5;
+// let b = 2;
+// console.log("a + b = ",a + b);
+
+// console.log("a - b = ",a - b);
+
+// console.log("a * b = ",a * b);
+
+// console.log("a / b = ",a / b);
+ 
+// console.log("a % b = ",a % b);
+
+// console.log("a ** b = ",a ** b);
+
+// console.log("a ++ b = ",a ++ b);
+
+// let a = 5;
+// let b = 2;
+// console.log("a + b = ",a + b);
+
+// console.log("a - b = ",a - b);
+
+// console.log("a * b = ",a * b);
+
+// console.log("a / b = ",a / b);
+
+// console.log("a % b = ",a % b);
+
+// console.log("a ** b = ",a ** b);
+
+// console.log("a + b = ",a + b);
+
+
+// console.log("a++ = ",a );
+
+// console.log("a-- = ",a );
+
+// console.log("++a = ",a );
+
+// console.log("--a = ",a );
+Teacher:ER sajan kafle
+Website:theme forest 
+3 month
+1.git & github
+2.javascript
+3.typescript
+4.nodejs
+
+
+1.html
+2.css         
+3.tailwind css/boostrap/Ant design
+4.React js& redux
+project 1. news Portal 
+        2.ecommerce
+ 
+next js
+       1 Ecommerce
